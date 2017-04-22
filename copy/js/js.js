07cb35c7bd2707f869354ffef3c998469c9876db
thedeltaproject <thedeltaproject@users.noreply.github.com>
2017-04-23 01:20:47 +0300

    Upd

--- copy/js/js.js
+++ copy/js/js.js
@@ -1,21 +1,28 @@
 "use strict";
 
-// Get the modal
-var modal = document.getElementById('myModal');
+// Visible width and height
+// var viewportWidth = window.innerWidth;
+// var viewportHeight = window.innerHeight;
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
+// ###################################################
+// Modal image #######################################
+// ###################################################
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById("modal-img");
+
+modal.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Modal images initialisation
+var zoomImgList = document.getElementsByClassName("zoom-img");
+var i;
+for (i = 0; i < zoomImgList.length; i++) {
+  zoomImgList[i].onclick = function(){
+    modal.style.display = "flex";
     modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+  }
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// ###################################################
+// ###################################################
+// ###################################################
